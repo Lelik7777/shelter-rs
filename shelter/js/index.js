e0f36e83ab6c0c6e029921dad97656b78cbca13a
Lelik Jan <21alex7777@gmail.com>
2023-04-09 11:26:43 +0300

    feat: make burger mechanism

--- shelter/js/index.js
+++ shelter/js/index.js
@@ -1,2 +1,22 @@
-const $burger=document.querySelector('.header__burger');
+const $burger = document.querySelector('.header__burger');
+const $burgerLine = document.querySelector('.burger__line');
+const $headerNav = document.querySelector('.header__navigation');
+const $overlay = document.querySelector('.overlay');
+
+const active = '_active';
+const lock = '_lock';
+
+
+//burger menu mechanism
+[$burger, $overlay].forEach(el => {
+    el.addEventListener('click', function () {
+        $burger.classList.toggle(active);
+        $burgerLine.classList.toggle(active);
+        $headerNav.classList.toggle(active);
+        document.body.classList.toggle(lock);
+        $overlay.classList.toggle(active);
+    })
+});
+
+
 
