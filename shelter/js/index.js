c86353a8b11f0a2c222c479ec37c2ca5882e7acb
Lelik Jan <21alex7777@gmail.com>
2023-04-09 12:59:09 +0300

    feat: add data with pets

--- shelter/js/index.js
+++ shelter/js/index.js
@@ -1,11 +1,15 @@
 const $burger = document.querySelector('.header__burger');
 const $burgerLine = document.querySelector('.burger__line');
 const $headerNav = document.querySelector('.header__navigation');
+const $navItems = document.querySelectorAll('.navigation__item');
 const $overlay = document.querySelector('.overlay');
-
 const active = '_active';
 const lock = '_lock';
 
+window.onload = function () {
+    const data = fetch('./data/pets.json').then(res => res.json())
+        .then(data => console.log(data));
+};
 
 //burger menu mechanism
 [$burger, $overlay].forEach(el => {
@@ -17,6 +21,14 @@ const lock = '_lock';
         $overlay.classList.toggle(active);
     })
 });
-
+$navItems.forEach(el => {
+    el.addEventListener('click', function () {
+        $burger.classList.toggle(active);
+        $burgerLine.classList.toggle(active);
+        $headerNav.classList.toggle(active);
+        document.body.classList.toggle(lock);
+        $overlay.classList.toggle(active);
+    });
+});
 
 
